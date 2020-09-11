3ea2b224da1661e88ed2631a825498b472b37bb7
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2020-09-11 14:00:31 -0700

    Add files via upload

---
+++ creepercraft_main.js
@@ -0,0 +1,66 @@
+//LScribe LScript encodingURI 3310 build 2010. Built with Unix 2010
+enc
+    encodeURI{3310}
+yield [WebGLTransformFeedback]
+    WebAssembly
+        WebAuthentication{WeakMap}
+        {
+            frameElement
+                frames
+        }
+encodeLayer
+        layerFist{3}
+    TextEncoderStream
+        enc
+            FileList
+                fist 
+    URIError{if}
+        false
+    true
+        JTempest
+            encodeURIComponent
+    enc
+    falseVideo (encodeLayer)
+javaRE -JRE ReadableStream
+        JRE -runtime 555 HTMLSourceElement{unit}{hour} 
+            sidestream{Element}={60 p Uint16Array} -unit
+                Uint32Array
+                    decodeURIComponent
+    URIError backend
+        {VarDate}var (3)
+            parseInt(s:toString)
+        SVGAnimatedString
+            string
+falseParsal
+        trueParsal
+            parsal[1]
+            DOMMatrixReadOnly{1}
+        parsal[2]
+        DOMMatrixReadOnly{2}
+            parseFloat{parsal}
+doNotTrack
+    do {
+        
+    } while (parsal[sep]);
+{
+    {
+        inject[PaymentAddress]load(2)
+    }
+}
+void
+    break(ProcessingInstruction)
+    BeforeUnloadEvent
+        break
+unfold
+    BroadcastChannel{1}
+        void
+    value: queueMicrotask
+        VRFrameData
+break
+    webkitRTCPeerConnection{WebKitCSSMatrix}
+        WebAssembly
+            webDomain{domain.com}HTMLBRElement
+    break
+
+
+
