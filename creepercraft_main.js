b9876133138e50f5d4830457d24578e6ca46133f
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2020-09-12 10:57:48 -0700

    Add files via upload

--- creepercraft_main.js
+++ creepercraft_main.js
@@ -1,3 +1,5 @@
+const { teal, tan } = require("color-name")
+
 //LScribe LScript encodingURI 3310 build 2010. Built with Unix 2010
 enc
     encodeURI{3310}
@@ -54,13 +56,41 @@ void
 unfold
     BroadcastChannel{1}
         void
-    value: queueMicrotask
+    value (queueMicrotask)
         VRFrameData
 break
     webkitRTCPeerConnection{WebKitCSSMatrix}
         WebAssembly
             webDomain{domain.com}HTMLBRElement
     break
-
+yield string (statusbar)
+status 
+    HTMLBaseElement{sidestream}
+        var :p: parse
+        {
+            condition for InputEvent{import { parsal } from "module";}
+            break
+        }
+conditionResolve {MediaError}
+        ReadableStream{readOnly}
+setMatch cos{AudioWorklet}
+        var{graphic}
+    SpeechGrammar
+        SpeechGrammarList{outputFalse}
+    outputTrue
+        bias{1}
+ConvolverNode
+        nodename("CreeperCraft SMP Server. A branch of the BitBlock Network")
+    TextDecoderStream
+        RTCSessionDescription nodename("CreeperCraft. A BitBlock Network SMP Survival Server. Made by sbirdo#7219 and Hosted by DankRadio#0001")
+nodeReadOnly
+        NaN
+    NodeList
+        ConvolverNode
+execute{for var (3) NodeList}
+Node {NaN}
+isNaN
+        break
+        
 
 
