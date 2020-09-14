87009f2006fa05454de9a948932133c6b2d437fb
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2020-09-13 20:36:42 -0700

    seed gen

---
+++ CreeperCraft_seedDrag.js
@@ -0,0 +1,9 @@
+{
+    resolveNode{styleMedia}
+    seed("-8913466909937400889")
+        matchMedia
+}
+    AbortController
+        Node[NodeFilter]
+            false for (VarDate)
+                var
